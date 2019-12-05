417535b26171dcec83bea349af0440bfd32537ef
David Souza <david.jsouzaa@gmail.com>
2019-12-05 06:32:38 -0300

    adding more css and javascript

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function changeBlue () {
     document.getElementById("change-from-js").style.backgroundColor = "blue";
 }
 
-
+window.onload = function() {
+    var btn = document.createElement("BUTTON");   
+    btn.innerHTML = "CLICK ME";                 
+    document.body.appendChild(btn); 
+};
